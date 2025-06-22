fb6bff4b01776da17207eabcc7c042d955beb083
AstridVides-cyber <vkirito67@gmail.com>
2025-06-21 18:11:42 -0600

    Modificando el modelo de restaurante

---
+++ src/models/restaurante.model.js
@@ -0,0 +1,74 @@
+import mongoose, { Schema, model } from 'mongoose';
+
+const restaurantSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: false,
+    },
+    direction: {
+        type: String,
+        required: true,
+    },
+    idCoordinate: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Coordinate",
+        required: false,
+    },
+    idPictures: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Picture',
+        required: false,
+    },
+    idMenu: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Menu',
+        required: false,
+    },
+    idPromotion: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Promotion',
+        required: false,
+    },
+    idUser: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    idReviews: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Review',
+        required: false,
+    },
+    idBlocks: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Block',
+        required: false,
+    },
+    idTimeRange: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'timeRange',
+        required: false,
+    },
+    onWait: {
+        type: Boolean,
+        required: true,
+    }
+});
+
+export const Restaurant = model('Restaurant', restaurantSchema);
