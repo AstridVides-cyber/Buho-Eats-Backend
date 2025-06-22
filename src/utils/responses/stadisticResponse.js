43ed8a90d0f0ded51c7bdb8ed8f68f84d47138c6
AstridVides-cyber <vkirito67@gmail.com>
2025-06-21 19:44:45 -0600

    Agregando utils de responses

---
+++ src/utils/responses/stadisticResponse.js
@@ -0,0 +1,8 @@
+export default class StadisticResponse {
+    constructor(views, reviews, favorites, qualification) {
+        this.views = views;
+        this.reviews = reviews;
+        this.favorites = favorites;
+        this.qualification = qualification;
+    }
+};
