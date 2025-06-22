a480bc17cbde4c3190486f4f2c9bb85697edfb2c
AstridVides-cyber <vkirito67@gmail.com>
2025-06-21 22:56:53 -0600

    Actualizando validator de favorito

--- src/validators/favorite.validator.js
+++ src/validators/favorite.validator.js
@@ -1,43 +1,36 @@
 import { body, param } from "express-validator";
 import { validateResult } from "../utils/helpers/validate.helper.js";
 
-// Validación para crear un favorito
-export const validateCreateFavorite = [
-    body("idUser")
+// Validación para agregar restaurante a favoritos
+export const validateAddRestaurantToFavorites = [
+    param("id")
         .exists()
         .isMongoId()
-        .withMessage("El id del usuario es obligatorio y debe ser un ID válido de Mongo"),
+        .withMessage("El id del usuario debe ser un ObjectId válido"),
+
     body("idRestaurant")
         .exists()
         .isMongoId()
-        .withMessage("El id del restaurante es obligatorio y debe ser un ID válido de Mongo"),
+        .withMessage("El id del restaurante debe ser un ObjectId válido"),
+
     (req, res, next) => {
         validateResult(req, res, next);
-    }
+    },
 ];
 
-// Validación para obtener los favoritos de un usuario
-export const validateGetFavoritesByUser = [
-    param("idUser")
+// Validación para eliminar restaurante de favoritos
+export const validateRemoveRestaurantFromFavorites = [
+    param("id")
         .exists()
         .isMongoId()
-        .withMessage("El id del usuario es obligatorio y debe ser un ID válido de Mongo"),
-    (req, res, next) => {
-        validateResult(req, res, next);
-    }
-];
+        .withMessage("El id del usuario debe ser un ObjectId válido"),
 
-// Validación para eliminar un favorito
-export const validateDeleteFavorite = [
-    param("idUser")
+    param("restaurantId")
         .exists()
         .isMongoId()
-        .withMessage("El id del usuario es obligatorio y debe ser un ID válido de Mongo"),
-    param("idRestaurant")
-        .exists()
-        .isMongoId()
-        .withMessage("El id del restaurante es obligatorio y debe ser un ID válido de Mongo"),
+        .withMessage("El id del restaurante debe ser un ObjectId válido"),
+
     (req, res, next) => {
         validateResult(req, res, next);
-    }
+    },
 ];
