af192723858844752785c660a9d1c81dd344c142
AstridVides-cyber <vkirito67@gmail.com>
2025-06-21 18:39:56 -0600

    Agregando service de block

---
+++ src/services/block.service.js
@@ -0,0 +1,25 @@
+import { Block } from "../models/block.model.js";
+
+// Crear un bloqueo
+export const createBlock = async (restaurantId, userId) => {
+    try {
+        const existingBlock = await Block.findOne({ blocker: restaurantId, blocked: userId });
+        if (existingBlock) throw new Error("El usuario ya está bloqueado");
+        
+        const newBlock = new Block({ blocker: restaurantId, blocked: userId });
+        await newBlock.save();
+        return newBlock;
+    } catch (error) {
+        throw new Error(`Error al bloquear al usuario: ${error.message}`);
+    }
+};
+
+// Desbloquear un usuario
+export const deleteBlock = async (restaurantId, userId) => {
+    try {
+        const removedBlock = await Block.findOneAndDelete({ blocker: restaurantId, blocked: userId });
+        return removedBlock;
+    } catch (error) {
+        throw new Error(`Error al desbloquear al usuario: ${error.message}`);
+    }
+};
