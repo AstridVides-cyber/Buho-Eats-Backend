83eb8869731569ef9f8d2d443e8767fbc5125a06
AstridVides-cyber <vkirito67@gmail.com>
2025-06-21 22:30:00 -0600

    Actualizando controlador de favorito

--- src/controllers/favorite.controllers.js
+++ src/controllers/favorite.controllers.js
@@ -1,42 +1,33 @@
-import { 
-    createFavorite, 
-    getFavoritesByUser, 
-    removeFavorite 
-} from "../services/favorite.service.js";
+import { createFavorite, getFavoritesByUserId, deleteFavorite } from "../services/favorite.service.js";
 import createError from "http-errors";
 
-// Crear un nuevo favorito
+// Crear favorito
 export const createFavoriteController = async (req, res, next) => {
-    const { idUser, idRestaurant } = req.body;
-
+    const data = req.body;
     try {
-        const favorite = await createFavorite(idUser, idRestaurant);
-        res.status(201).json({ message: "Favorito agregado correctamente", data: favorite });
+        const createdFavorite = await createFavorite(data);
+        res.status(201).json({ message: "Favorito creado con éxito", data: createdFavorite });
     } catch (error) {
         next(error);
     }
 };
 
-// Obtener los favoritos de un usuario
-export const getFavoritesByUserController = async (req, res, next) => {
+// Obtener favoritos de un usuario
+export const getFavoritesByUserIdController = async (req, res, next) => {
     const { idUser } = req.params;
-
     try {
-        const favorites = await getFavoritesByUser(idUser);
-        if (!favorites) throw createError(404, "No se encontraron favoritos para este usuario");
-        res.status(200).json({ data: favorites });
+        const favorites = await getFavoritesByUserId(idUser);
+        res.status(200).json({ message: "Favoritos obtenidos", data: favorites });
     } catch (error) {
         next(error);
     }
 };
 
 // Eliminar un favorito
-export const removeFavoriteController = async (req, res, next) => {
-    const { idUser, idRestaurant } = req.params;
-
+export const deleteFavoriteController = async (req, res, next) => {
+    const { idUser, favoriteId } = req.params;
     try {
-        const favorite = await removeFavorite(idUser, idRestaurant);
-        if (!favorite) throw createError(404, "No se encontró el favorito para eliminar");
+        await deleteFavorite(idUser, favoriteId);
         res.status(200).json({ message: "Favorito eliminado correctamente" });
     } catch (error) {
         next(error);
