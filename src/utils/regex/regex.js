3486845148cf06bf1767dc0bae21af7664720145
AstridVides-cyber <vkirito67@gmail.com>
2025-06-21 19:43:41 -0600

    Agregando utils de regex

---
+++ src/utils/regex/regex.js
@@ -0,0 +1,6 @@
+//Contraseña
+export const PASSWORD = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.{6,24})/;
+//Imagen
+export const PICTURE = /^https?:\/\/.*\.(jpg|jpeg|png)$/;
+//authcode
+export const AUTHCODE = /^[A-Za-z0-9-_]+$/;
