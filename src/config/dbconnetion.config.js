8b3580ab08b4b51042b8cd2f080ff2205995e170
AstridVides-cyber <vkirito67@gmail.com>
2025-06-21 19:32:23 -0600

    Agregando conetion

---
+++ src/config/dbconnetion.config.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import 'dotenv/config';
+
+export const connectiondb = async () => {
+    const URI = process.env.MONGODB_URI;
+
+    if (!URI) throw new Error('No se pudo conectar con la base de datos:(');
+
+    try {
+        await mongoose.connect(URI);
+        console.log("Base datos conectada:D")
+    } catch (error) {
+        console.error(error);
+        throw new Error("Error al iniciar la base de datos:(");
+        
+    }
+}
