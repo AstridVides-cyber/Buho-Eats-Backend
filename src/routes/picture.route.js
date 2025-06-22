929ff77b0e0e60d13ff199ccf3302401d0d0db90
AstridVides-cyber <vkirito67@gmail.com>
2025-06-21 19:06:01 -0600

    Agregando router de pocture

---
+++ src/routes/picture.route.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { 
+    createPictureController, 
+    getAllPicturesController, 
+    getPictureByIdController, 
+    deletePictureByIdController 
+} from "../controllers/picture.controllers.js";
+
+const pictureRouter = Router();
+
+// Crear una nueva imagen
+pictureRouter.post('/create', createPictureController);
+
+// Obtener todas las imágenes
+pictureRouter.get('/all', getAllPicturesController);
+
+// Obtener una imagen específica por ID
+pictureRouter.get('/:id', getPictureByIdController);
+
+// Eliminar una imagen por ID
+pictureRouter.delete('/:id', deletePictureByIdController);
+
+export { pictureRouter };
