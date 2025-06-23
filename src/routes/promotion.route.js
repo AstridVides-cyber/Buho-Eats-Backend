0bb1c8aee0fb63831e64533a67496c222f3d0475
AstridVides-cyber <vkirito67@gmail.com>
2025-06-23 17:10:15 -0600

    Modificando ruta promocion

--- src/routes/promotion.route.js
+++ src/routes/promotion.route.js
@@ -1,27 +1,16 @@
 import { Router } from "express";
 import {
     createPromotionController,
-    findAllPromotionsController,
-    findPromotionByIdController,
-    updatePromotionController,
+    getAllPromotionsController,
+    getPromotionsByRestaurantController,
     deletePromotionController
 } from "../controllers/promotion.controllers.js";
 
 const promotionRouter = Router();
 
-// Crear una nueva promoción
-promotionRouter.post("/create", createPromotionController);
-
-// Obtener todas las promociones
-promotionRouter.get("/all", findAllPromotionsController);
-
-// Obtener una promoción por su ID
-promotionRouter.get("/:id", findPromotionByIdController);
-
-// Actualizar una promoción
-promotionRouter.put("/:id", updatePromotionController);
-
-// Eliminar una promoción
-promotionRouter.delete("/:id", deletePromotionController);
+promotionRouter.post("/create", createPromotionController); // Crear promoción
+promotionRouter.get("/all", getAllPromotionsController); // Obtener todas las promociones
+promotionRouter.get("/restaurant/:restaurantId", getPromotionsByRestaurantController); // Obtener promociones por restaurante
+promotionRouter.delete("/:id", deletePromotionController); // Eliminar una promoción
 
 export { promotionRouter };
