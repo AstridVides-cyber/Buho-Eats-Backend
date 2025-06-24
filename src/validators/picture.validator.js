e670230ac76d52fc825281c86966c35809499d87
AstridVides-cyber <vkirito67@gmail.com>
2025-06-24 14:33:20 -0600

    Modificando ruta de picture

--- src/validators/picture.validator.js
+++ src/validators/picture.validator.js
@@ -1,59 +1,59 @@
-import path from "path";
 import { body, param } from "express-validator";
 import { validateResult } from "../utils/helpers/validate.helper.js";
 import { PICTURE } from "../utils/regex/regex.js";
 
-// Validación para crear una imagen
+//Validacion de crear imagen
 export const validateCreatePicture = [
     param("idRestaurant")
         .exists()
         .notEmpty()
         .isMongoId()
-        .withMessage("El idRestaurant debe ser un ID de MongoDB válido"),
+        .withMessage("El idRestaurant debe ser un ID de Mongo valido"),
 
     body("url")
         .exists()
-        .notEmpty()
-        .isString()
+        .isArray()
         .withMessage("La URL de la imagen es obligatoria")
         .matches(PICTURE)
-        .withMessage("La URL de la imagen debe tener un formato válido y una extensión jpg, jpeg o png"),
-
-    (req, res, next) => {
-        validateResult(req, res, next);  
-    }
-];
-
-//Validacion para agregar imagenes
-export const validateAddPictures = [
-    body("id")
-        .exists()
-        .notEmpty()
-        .isMongoId()
-        .withMessage("El id debe ser un ID de Mongo válido"),
-
-    body("url").custom((value, { req }) => {
-        if (!req.files || req.files.length === 0) {
-            throw new Error("Debe enviar al menos una imagen");
-        }
-        req.files.forEach((file) => {
-            const validExtensions = [".jpg", ".jpeg", ".png"];
-            const fileExtension = path.extname(file.originalname);
-            if (!validExtensions.includes(fileExtension)) {
-                throw new Error(
-                    `Solo se permiten imágenes con las extensiones: ${validExtensions.join(", ")}`
-                );
-            }
-        });
-        return true;
-    }),
+        .withMessage("La imagen deber ser de extension jpg, jpeg o png"),
 
     (req, res, next) => {
         validateResult(req, res, next);
     },
 ];
 
-//Validacion para eliminar multiples imagenes
+//Validacion al agregar imagen
+export const validateAddPictures = [
+    body("id")
+        .exists()
+        .notEmpty()
+        .isMongoId()
+        .withMessage("El id debe ser un ID de Mongo valido"),
+
+    body("url").custom((value, { req }) => {
+        if (!req.files || req.files.length === 0) {
+        throw new Error("Debe enviar al menos una imagen");
+        }
+        req.files.forEach((file) => {
+        const validExtensions = [".jpg", ".jpeg", ".png"];
+        const fileExtension = path.extname(file.originalname);
+        if (!validExtensions.includes(fileExtension)) {
+            throw new Error(
+            `Solo se permiten imágenes con las extensiones: ${validExtensions.join(
+                ", "
+            )}`
+            );
+        }
+    });
+    return true;
+}),
+
+    (req, res, next) => {
+        validateResult(req, res, next);
+    },
+];
+
+//Validacion de remover imagenes
 export const validateRemovePictures = [
     body("id")
         .exists()
@@ -68,11 +68,11 @@ export const validateRemovePictures = [
         .withMessage("Debe enviar al menos una imagen para eliminar"),
 
     (req, res, next) => {
-        validateResult(req, res, next);
+    validateResult(req, res, next);
     },
 ];
 
-//Validacion para eliminar una imagen
+//Validacion de eliminar imagen
 export const validateDeletePicture = [
     body("id")
         .exists()
