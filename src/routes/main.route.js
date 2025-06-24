d8dff68d80b914b52c608c74a727d258b1ac0eb7
AstridVides-cyber <vkirito67@gmail.com>
2025-06-24 00:45:38 -0600

    Modificando ruta de main

--- src/routes/main.route.js
+++ src/routes/main.route.js
@@ -14,56 +14,39 @@ import { coordinateRouter } from "./coordinate.route.js";
 const mainRouter = Router();
 
 // Ruta para los usuarios:D
-// /api/usuario
 mainRouter.use('/api/usuario', userRouter); 
 
 // Ruta para los favoritos de un usuario:D
-// /api/usuario/:id/favoritos
 mainRouter.use('/api/usuario/:id/favoritos', favoriteRouter); 
 
 // Ruta para restaurantes:D
-// /api/restaurante
 mainRouter.use('/api/restaurante', restaurantRouter); 
 
 // Ruta para el menú de un restaurante:D
-// /api/restaurante/:id/menu
 mainRouter.use('/api/restaurante/:restaurantId/menu', menuRouter); 
 
 // Promociones Rutas:D
-///api/restaurante/id/promocion/
-mainRouter.use('/api/restaurante', promotionRouter);
+mainRouter.use('/api/restaurante/:restaurantId/promocion', promotionRouter);
 
 // Bloqueo Rutas:D
-// /api/restaurante/:id/bloquear
-mainRouter.use('/api/restaurante/:id/bloquear', blockRouter);
+mainRouter.use('/api/restaurante/:restaurantId/bloquear', blockRouter);
 
 // Rutas de reseñas:D
-// /api/restaurante/:id/review
-mainRouter.use('api/restaurante/:id/review', reviewRouter); 
-
+mainRouter.use('/api/restaurante/:restaurantId/review', reviewRouter); 
 
 // Ruta para los platos dentro de un menú
-mainRouter.use('/api/restaurante/:id/menu/plato', plateRouter);
-
-
+mainRouter.use('/api/restaurante/:restaurantId/menu/plato', plateRouter);
 
 // Ruta para las imágenes
+// /api/picture
 mainRouter.use('/api/picture', pictureRouter); 
 
 // Agregar rutas de TimeRange:D
+// /api/timeRange
 mainRouter.use("/api/timeRange", timeRangeRouter); 
 
 // Rutas de coordenadas:D
+// /api/coordenadas
 mainRouter.use("/api/coordenadas", coordinateRouter);
-
-
-
-
-
-
-
-
-
-
 
 export { mainRouter };
