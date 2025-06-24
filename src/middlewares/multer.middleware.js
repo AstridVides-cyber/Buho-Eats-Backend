9fdd07edd5ed9951b30e63782ab8c98037ca8d00
AstridVides-cyber <vkirito67@gmail.com>
2025-06-24 16:29:56 -0600

    Modificando multer

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,29 +1,27 @@
 import multer from 'multer';
 import path from 'path';
 
-// Configurar almacenamiento de imágenes
+//Configuracion del almacenamiento de las imagenes
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, './uploads');
+        cb(null, './uploads')
     },
     filename: (req, file, cb) => {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
 });
 
-// Filtro de tipo de archivo
 const fileFilter = (req, file, cb) => {
     const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg'];
-    if (allowedTypes.includes(file.mimetype)) {
+    if(allowedTypes.includes(file.mimetype)) {
         cb(null, true);
     } else {
         cb(new Error('Formato de archivo no permitido'), false);
     }
 };
 
-// Configuración de multer
 export const upload = multer({
     storage: storage,
-    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+    limits: { fileSize: 5 * 1024 * 1024 },
     fileFilter: fileFilter
 });
