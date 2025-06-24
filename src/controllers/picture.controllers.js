c8d2b170b71fc8ae59634a06ade99c571d07c44e
AstridVides-cyber <vkirito67@gmail.com>
2025-06-24 16:01:24 -0600

    Modificando controlador de picture

--- src/controllers/picture.controllers.js
+++ src/controllers/picture.controllers.js
@@ -1,82 +1,149 @@
-import { 
-    saveImage, 
-    addPictures, 
-    removePictures, 
-    findAllPictures, 
-    findPictureById, 
-    deletePictureById 
+import { findRestaurantById } from "../services/restaurante.service.js";
+import {
+    addPictures,
+    deletePictureById,
+    findAllPictures,
+    findPictureById,
+    removePictures,
+    saveImage,
 } from "../services/picture.service.js";
+import fs from "fs";
+import path from "path";
+import { dirname } from "path";
 import createError from "http-errors";
+import { fileURLToPath } from "url";
 
-// Guardar nueva imagen
-export const saveImageController = async (req, res, next) => {
-    const { url, idRestaurant } = req.body; // Extraer los datos del cuerpo de la solicitud
+//Para obtener las rutas
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+//Crear la imagen
+export const createPictureController = async (req, res, next) => {
+    const { url } = req.body;
+    const { idRestaurant } = req.params;
 
     try {
+        const existRestaurant = await findRestaurantById(idRestaurant);
+
+        if (!existRestaurant) throw createError(404, "No se encontro el Restaurant");
+
         const picture = await saveImage(url, idRestaurant);
-        res.status(201).json({ success: true, message: "Imagen creada correctamente", data: picture });
+
+        res
+        .status(201)
+        .json({ message: "Se creo la imagen para el Restaurant", data: picture });
     } catch (error) {
         next(error);
     }
 };
 
-// Agregar imágenes a un Restaurant
+//Agrega imagen
 export const addPicturesController = async (req, res, next) => {
-    const { picturesToAdd } = req.body; // Extraer las imágenes a agregar
-    const { id } = req.params; // Extraer el id del Restaurant desde los parámetros de la URL
-
     try {
-        const updatedRestaurant = await addPictures(picturesToAdd, id);
-        res.status(200).json({ success: true, message: "Imágenes agregadas correctamente", data: updatedRestaurant });
+        const { id } = req.body;
+
+        //Guarda en las variables un objeto que contiene el atributo url
+        const fileName = req.files.map((file) => ({
+        url: file.filename,
+        }));
+
+        //Se mapea para extraer la url y guardarla en un arreglo de string
+        const urls = fileName.map((picture) => picture.url);
+
+        const existPicture = await findPictureById(id);
+
+        if (!existPicture)
+        throw new createError(404, "No se encontraron las imagenes");
+
+        await addPictures(urls, id);
+
+        res.status(200).json({ message: "Se agregaron las imagenes" });
     } catch (error) {
         next(error);
     }
 };
 
-// Eliminar imágenes de un Restaurant
+//Remover imagen
 export const removePicturesController = async (req, res, next) => {
-    const { picturesToRemove } = req.body; // Extraer las imágenes a eliminar
-    const { id } = req.params; // Extraer el id del Restaurant desde los parámetros de la URL
+    const { url, id } = req.body;
 
     try {
-        const updatedRestaurant = await removePictures(picturesToRemove, id);
-        res.status(200).json({ success: true, message: "Imágenes eliminadas correctamente", data: updatedRestaurant });
+        const existPicture = await findPictureById(id);
+
+        if (!existPicture)
+        throw new createError(404, "No se encontraron las imagenes");
+
+        const removed = await removePictures(url, id);
+
+        //Filtra las imagenes que se van a eliminar, busca si estan en la base de datos para asi asegurar de que se eliminaran solo
+        //elementos que se encuentren en la base de datos
+        const urlsToDelete = existPicture.url.filter((urls) => url.includes(urls));
+
+        //Si urlsToDelete esta vacion significa que no habia ninguno de los elementos que se querian borrar en ella, por lo que tira un error
+        if (urlsToDelete.length === 0)
+        throw new createError(404, "No se encontraron las imagenes");
+
+        //Recorre con un foreach para ir eliminando cada elemento del arreglo de urlsToDelete
+        urlsToDelete.forEach((url) => {
+        const filePath = path.join(__dirname, "..", "..", "uploads", url);
+        //fs se utiliza para eliminar en la carpeta los archivos que coincidan con los de la url
+        fs.unlink(filePath, (error) => {
+            if (error) {
+            throw new createError(
+                500,
+                `Error al eliminar la imagen ${url}: ${error.message}`
+            );
+            }
+        });
+    });
+
+    res.status(200).json({ message: "Se eliminaron las imagenes" });
     } catch (error) {
         next(error);
     }
 };
 
-// Obtener todas las imágenes
-export const findAllPicturesController = async (req, res, next) => {
+//Obtener imagenes
+export const getPicturesController = async (req, res, next) => {
     try {
         const pictures = await findAllPictures();
-        res.status(200).json({ success: true, data: pictures });
+
+        if (!pictures)
+        throw createError(404, "No se encontraron imagenes del Restaurant");
+
+        res.status(200).json({ data: pictures });
     } catch (error) {
         next(error);
     }
 };
 
-// Obtener una imagen por su ID
+//Obtener las imagenes
 export const findPictureByIdController = async (req, res, next) => {
-    const { id } = req.params; // Extraer el id de la imagen desde los parámetros de la URL
-
     try {
+        const { id } = req.params;
+
         const picture = await findPictureById(id);
-        if (!picture) throw new createError(404, "Imagen no encontrada");
-        res.status(200).json({ success: true, data: picture });
+
+        if (!picture) throw new createError(404, "No se encontraron las imagenes");
+
+        res.status(200).json({ data: picture });
     } catch (error) {
         next(error);
     }
 };
 
-// Eliminar una imagen por su ID
-export const deletePictureByIdController = async (req, res, next) => {
-    const { id } = req.params; // Extraer el id de la imagen desde los parámetros de la URL
-
+//Eliminar imagen
+export const deletePictureController = async (req, res, next) => {
     try {
+        const { id } = req.body;
+
         const deletedPicture = await deletePictureById(id);
-        if (!deletedPicture) throw new createError(404, "Imagen no encontrada");
-        res.status(200).json({ success: true, message: "Imagen eliminada correctamente" });
+
+        if (!deletedPicture) throw new createError(404, "No se encontro la imagen");
+
+        res
+        .status(200)
+        .json({ message: "Se eliminaron las imagenes exitosamente" });
     } catch (error) {
         next(error);
     }
