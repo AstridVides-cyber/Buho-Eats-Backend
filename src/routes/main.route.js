d820a63ca9a0b365bf242f339e6311c4da8023a3
AstridVides-cyber <vkirito67@gmail.com>
2025-06-24 02:21:21 -0600

    Modificando ruta de main

--- src/routes/main.route.js
+++ src/routes/main.route.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { userRouter } from "./user.route.js";
 import { restaurantRouter } from "./restaurante.route.js";
 import { pictureRouter } from "./picture.route.js";//Falta
-import { reviewRouter } from "./review.route.js";
+import { reviewRouter } from "./review.route.js";//Falta
 import { favoriteRouter } from "./favorite.route.js";
 import { promotionRouter } from "./promotion.route.js";
 import { menuRouter } from "./menu.route.js";//Falta
@@ -22,9 +22,6 @@ mainRouter.use('/api/usuario/:id/favoritos', favoriteRouter);
 // Ruta para restaurantes:D
 mainRouter.use('/api/restaurante', restaurantRouter); 
 
-// Ruta para el menú de un restaurante
-mainRouter.use('/api/restaurante/:restaurantId/menu', menuRouter); 
-
 // Promociones Rutas:D
 mainRouter.use('/api/restaurante/:restaurantId/promotion', promotionRouter);
 
@@ -34,19 +31,21 @@ mainRouter.use('/api/restaurante/:restaurantId/bloquear', blockRouter);
 // Rutas de reseñas:D
 mainRouter.use('/api/restaurante/:restaurantId/review', reviewRouter); 
 
+// Agregar rutas de TimeRange:D
+mainRouter.use("/api/timeRange", timeRangeRouter); 
+
+// Rutas de coordenadas:D
+mainRouter.use("/api/coordenadas", coordinateRouter);
+
+
+
+// Ruta para el menú de un restaurante
+mainRouter.use('/api/restaurante/:restaurantId/menu', menuRouter); 
+
 // Ruta para los platos dentro de un menú
 mainRouter.use('/api/restaurante/:restaurantId/menu/plato', plateRouter);
 
 // Ruta para las imágenes
-// /api/picture
 mainRouter.use('/api/picture', pictureRouter); 
-
-// Agregar rutas de TimeRange:D
-// /api/timeRange
-mainRouter.use("/api/timeRange", timeRangeRouter); 
-
-// Rutas de coordenadas:D
-// /api/coordenadas
-mainRouter.use("/api/coordenadas", coordinateRouter);
 
 export { mainRouter };
