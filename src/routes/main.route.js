c1e515267860a059bd55f4a542ad889720bb1457
AstridVides-cyber <vkirito67@gmail.com>
2025-06-24 00:06:07 -0600

    Modificando ruta de promo

--- src/routes/main.route.js
+++ src/routes/main.route.js
@@ -14,31 +14,44 @@ import { coordinateRouter } from "./coordinate.route.js";
 const mainRouter = Router();
 
 // Ruta para los usuarios:D
+// /api/usuario
 mainRouter.use('/api/usuario', userRouter); 
 
 // Ruta para los favoritos de un usuario:D
+// /api/usuario/:id/favoritos
 mainRouter.use('/api/usuario/:id/favoritos', favoriteRouter); 
 
 // Ruta para restaurantes:D
+// /api/restaurante
 mainRouter.use('/api/restaurante', restaurantRouter); 
 
-// Promociones Rutas:D
-mainRouter.use('/api/restaurante', promotionRouter);
-
 // Ruta para el menú de un restaurante:D
+// /api/restaurante/:id/menu
 mainRouter.use('/api/restaurante/:restaurantId/menu', menuRouter); 
 
+// Promociones Rutas:D
+///api/restaurante/id/promocion/
+mainRouter.use('/api/restaurante', promotionRouter);
+
+// Bloqueo Rutas:D
+// /api/restaurante/:id/bloquear
+mainRouter.use('/api/restaurante/:id/bloquear', blockRouter);
+
 // Rutas de reseñas:D
+// /api/restaurante/:id/review
 mainRouter.use('api/restaurante/:id/review', reviewRouter); 
+
 
 // Ruta para los platos dentro de un menú
 mainRouter.use('/api/restaurante/:id/menu/plato', plateRouter);
 
+
+
+// Ruta para las imágenes
+mainRouter.use('/api/picture', pictureRouter); 
+
 // Agregar rutas de TimeRange:D
 mainRouter.use("/api/timeRange", timeRangeRouter); 
-
-// Bloqueo Rutas:D
-mainRouter.use('/api/restaurante', blockRouter);
 
 // Rutas de coordenadas:D
 mainRouter.use("/api/coordenadas", coordinateRouter);
@@ -46,8 +59,9 @@ mainRouter.use("/api/coordenadas", coordinateRouter);
 
 
 
-// Ruta para las imágenes
-mainRouter.use('/api/picture', pictureRouter); 
+
+
+
 
 
 
