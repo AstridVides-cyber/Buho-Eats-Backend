d368692ac2a69560fc659a1104efaf890b50aa95
AstridVides-cyber <vkirito67@gmail.com>
2025-06-24 15:23:51 -0600

    Modificando controlador de picture

--- src/controllers/picture.controllers.js
+++ src/controllers/picture.controllers.js
@@ -10,37 +10,37 @@ import createError from "http-errors";
 
 // Guardar nueva imagen
 export const saveImageController = async (req, res, next) => {
-    const { url, idLocal } = req.body; // Extraer los datos del cuerpo de la solicitud
+    const { url, idRestaurant } = req.body; // Extraer los datos del cuerpo de la solicitud
 
     try {
-        const picture = await saveImage(url, idLocal);
+        const picture = await saveImage(url, idRestaurant);
         res.status(201).json({ success: true, message: "Imagen creada correctamente", data: picture });
     } catch (error) {
         next(error);
     }
 };
 
-// Agregar imágenes a un local
+// Agregar imágenes a un Restaurant
 export const addPicturesController = async (req, res, next) => {
     const { picturesToAdd } = req.body; // Extraer las imágenes a agregar
-    const { id } = req.params; // Extraer el id del local desde los parámetros de la URL
+    const { id } = req.params; // Extraer el id del Restaurant desde los parámetros de la URL
 
     try {
-        const updatedLocal = await addPictures(picturesToAdd, id);
-        res.status(200).json({ success: true, message: "Imágenes agregadas correctamente", data: updatedLocal });
+        const updatedRestaurant = await addPictures(picturesToAdd, id);
+        res.status(200).json({ success: true, message: "Imágenes agregadas correctamente", data: updatedRestaurant });
     } catch (error) {
         next(error);
     }
 };
 
-// Eliminar imágenes de un local
+// Eliminar imágenes de un Restaurant
 export const removePicturesController = async (req, res, next) => {
     const { picturesToRemove } = req.body; // Extraer las imágenes a eliminar
-    const { id } = req.params; // Extraer el id del local desde los parámetros de la URL
+    const { id } = req.params; // Extraer el id del Restaurant desde los parámetros de la URL
 
     try {
-        const updatedLocal = await removePictures(picturesToRemove, id);
-        res.status(200).json({ success: true, message: "Imágenes eliminadas correctamente", data: updatedLocal });
+        const updatedRestaurant = await removePictures(picturesToRemove, id);
+        res.status(200).json({ success: true, message: "Imágenes eliminadas correctamente", data: updatedRestaurant });
     } catch (error) {
         next(error);
     }
