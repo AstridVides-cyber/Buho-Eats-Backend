17bd2ce979f8c41d796ae49505106ccebb1be5d6
AstridVides-cyber <vkirito67@gmail.com>
2025-06-24 14:54:30 -0600

    Modificando ruta de picture

--- src/routes/picture.route.js
+++ src/routes/picture.route.js
@@ -5,19 +5,28 @@ import {
     getPictureByIdController, 
     deletePictureByIdController 
 } from "../controllers/picture.controllers.js";
+import { upload } from "../middleware/multer.middleware.js";
+import { 
+    validateAddPictures, 
+    validateCreatePicture, 
+    validateDeletePicture, 
+    validateRemovePictures 
+} from "../validators/picture.validator.js";
 
 const pictureRouter = Router();
 
 // Crear una nueva imagen
-pictureRouter.post('/create', saveImageController);
+pictureRouter.post("/create/:idRestaurant", validateCreatePicture, saveImageController);
 
 // Obtener todas las imágenes
-pictureRouter.get('/all', getAllPicturesController);
+pictureRouter.get("/getAll", getAllPicturesController);
 
 // Obtener una imagen específica por ID
-pictureRouter.get('/:id', getPictureByIdController);
+pictureRouter.get("/getOne/:id", getPictureByIdController);
 
 // Eliminar una imagen por ID
-pictureRouter.delete('/:id', deletePictureByIdController);
+pictureRouter.delete("/delete", validateDeletePicture, deletePictureController);
+
+pictureRouter.delete("/remove", validateRemovePictures,upload.array('url', 10), removePicturesController);
 
 export { pictureRouter };
