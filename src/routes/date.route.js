7103ea84efd251806ab1ca676ac40ddcf67263ba
AstridVides-cyber <vkirito67@gmail.com>
2025-06-30 16:58:57 -0600

    Modificando el token

--- src/routes/date.route.js
+++ src/routes/date.route.js
@@ -4,13 +4,14 @@ import {
     findTimeRangeByIdController 
 } from "../controllers/date.controllers.js";
 import { validateCreateTimeRange } from "../validators/date.validator.js";
+import { verifyToken } from "../middlewares/jwt.middleware.js";
 
 const timeRangeRouter = Router();
 
 // Ruta para crear un rango de tiempo
-timeRangeRouter.post("/create", validateCreateTimeRange, createTimeRangeController);
+timeRangeRouter.post("/create", verifyToken, validateCreateTimeRange, createTimeRangeController);
 
 // Ruta para obtener un rango de tiempo por ID
-timeRangeRouter.get("/:id", findTimeRangeByIdController);
+timeRangeRouter.get("/:id", verifyToken, findTimeRangeByIdController);
 
 export { timeRangeRouter };
