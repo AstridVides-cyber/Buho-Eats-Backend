47bcfa2cc191d0f4d3710508447b9cbf0ba6e5ae
AstridVides-cyber <vkirito67@gmail.com>
2025-06-30 16:58:20 -0600

    Modificando el token

--- src/routes/favorite.route.js
+++ src/routes/favorite.route.js
@@ -3,13 +3,14 @@ import {
     addRestaurantToFavoritesController,
     removeRestaurantFromFavoritesController 
 } from "../controllers/favorite.controllers.js";
+import { verifyToken } from "../middlewares/jwt.middleware.js";
 
 const favoriteRouter = Router();
 
 // **Agregar restaurante a favoritos de un usuario**
-favoriteRouter.post("/:id/favoritos/add", addRestaurantToFavoritesController);
+favoriteRouter.post("/:id/favoritos/add", verifyToken, addRestaurantToFavoritesController);
 
 // **Eliminar restaurante de los favoritos de un usuario**
-favoriteRouter.delete("/:id/favoritos/:restaurantId", removeRestaurantFromFavoritesController);
+favoriteRouter.delete("/:id/favoritos/:restaurantId", verifyToken, removeRestaurantFromFavoritesController);
 
 export { favoriteRouter };
