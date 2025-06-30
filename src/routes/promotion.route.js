cd37093757fc5da824167b89711be996e6b8505f
AstridVides-cyber <vkirito67@gmail.com>
2025-06-30 16:51:36 -0600

    Modificando el token

--- src/routes/promotion.route.js
+++ src/routes/promotion.route.js
@@ -11,19 +11,20 @@ import {
     validateUpdatePromotion, 
     validateDeletePromotion 
 } from "../validators/promotion.validator.js";
+import { verifyToken } from "../middlewares/jwt.middleware.js";
 
 const promotionRouter = Router();
 
 // Crear promoción para un restaurante 
-promotionRouter.post("/create", validateCreatePromotion, createPromotionController);
+promotionRouter.post("/create", verifyToken, validateCreatePromotion, createPromotionController);
 
 // Obtener una promoción por ID 
-promotionRouter.get("/:promotionId", validateGetPromotionById, getPromotionByIdController);
+promotionRouter.get("/:promotionId", verifyToken, validateGetPromotionById, getPromotionByIdController);
 
 // Actualizar una promoción 
-promotionRouter.put("/:promotionId", validateUpdatePromotion, updatePromotionController);
+promotionRouter.put("/:promotionId", verifyToken, validateUpdatePromotion, updatePromotionController);
 
 // Eliminar una promoción 
-promotionRouter.delete("/:promotionId", validateDeletePromotion, deletePromotionController);
+promotionRouter.delete("/:promotionId", verifyToken, validateDeletePromotion, deletePromotionController);
 
 export { promotionRouter };
