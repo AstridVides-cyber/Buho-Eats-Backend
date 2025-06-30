5e4bdcf77d6ff122f0b3ca7b742696f3b90683b8
AstridVides-cyber <vkirito67@gmail.com>
2025-06-30 16:56:43 -0600

    Modificando el token

--- src/routes/menu.route.js
+++ src/routes/menu.route.js
@@ -6,23 +6,24 @@ import {
     updateMenuController,
     deleteMenuController,
 } from "../controllers/menu.controllers.js";
-import { validateMenuId } from "../validators/menu.validator.js";  
+import { validateMenuId } from "../validators/menu.validator.js"; 
+import { verifyToken } from "../middlewares/jwt.middleware.js";
 
 const menuRouter = Router();
 
 // Crear un menú para un restaurante
-menuRouter.post("/create", createMenuController);
+menuRouter.post("/create", verifyToken, createMenuController);
 
 // Obtener todos los menús
-menuRouter.get("/all", getAllMenusController);
+menuRouter.get("/all", verifyToken, getAllMenusController);
 
 // Obtener un menú por ID
-menuRouter.get("/:menuId", getMenuByIdController);
+menuRouter.get("/:menuId", verifyToken, getMenuByIdController);
 
 // Actualizar un menú por ID
-menuRouter.put("/:menuId", validateMenuId, updateMenuController);
+menuRouter.put("/:menuId", verifyToken, validateMenuId, updateMenuController);
 
 // Eliminar un menú por ID
-menuRouter.delete("/:menuId", validateMenuId, deleteMenuController);
+menuRouter.delete("/:menuId", verifyToken, validateMenuId, deleteMenuController);
 
 export { menuRouter };
